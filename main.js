9a5d4e2d95b4737aae9aea68362f03e1e5c6b02f
Cosmo Kent <cosmolightfoot@gmail.com>
2019-01-17 15:34:06 -0800

    clean up

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
-var x = 12;
 
-if(x > 12) {
-    console.log('x is equal to', x);
-}
-else {
-    console.log('sorry, too small');
-}
 // 1. Initialize
 // 2. Condition
 // 3. Post Loop Action
+// Example of a "For Loop" below
 
-for(var i = 0; i < x; i++) {
+/*for(var i = 0; i < x; i++) {
     if(i % 2 === 0, i > 1) {
         console.log(i);
     }
 }
+*/
 
 function evenOrOdd() {
     var input = document.getElementById('number');
